e89d206517e9c24db2f9c4b145b870b7ab7e9f3a
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2020-09-14 22:50:18 -0400

    initializes menu and profile page components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import Layout from './components/layout/layout';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+
+// Pages
+import Profile from './pages/rest_profile';
+import Menu from './pages/rest_menu';
+
+// Components
+import Layout from './components/layout/layout';
 
 function App() {
   return (
@@ -10,6 +16,12 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => 
             <h1>Home Page Content</h1>
+          }/>
+          <Route exact path="/profile" render={() =>
+            <Profile />
+          }/>
+          <Route exact path="/menu" render={() =>
+            <Menu />
           }/>
         </Switch>
       </Layout>
