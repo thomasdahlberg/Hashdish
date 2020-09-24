45ba65a6e4e4da0ccbd5ab99cde0ba22a7eee273
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2020-09-23 22:41:50 -0400

    initializes and styles profile page

--- src/pages/rest_profile/rest_profile.jsx
+++ src/pages/rest_profile/rest_profile.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import styles from './rest_profile.module.css';
 
 const Profile = (props) => {
     return(
-        <div>
-            <h1>Restaurant Profile Page</h1>
+        <div className={styles.container}>
+            <section className={styles.header}>
+                <img className={styles.logo} src="./art-box-logo.svg" alt="restaurant logo"/>
+                <div className={styles.title}>
+                    <h1>Restaurant Name</h1>
+                    <h2>123 Test Street, Redwood, WA 12345</h2>
+                </div>
+            </section>
+            <section className={styles.info}>
+                <h3>Hours: Tuesday - Sunday 12pm - 10pm</h3>
+                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </section>
+            <section className={styles.gallery}>
+                <img src="./rest1.jpg" alt="1"/>
+                <img src="./rest2.jpg" alt="2"/>
+                <img src="./rest3.jpg" alt="3"/>
+            </section>
         </div>
     )
 }
