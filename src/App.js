13e37ef636278a9f73e036414eab2c07a859b690
ArthurRyoo <jene0523@gmail.com>
2020-09-26 19:54:44 -0700

    Add login and logout feature

--- src/App.js
+++ src/App.js
@@ -5,51 +5,58 @@ import './App.css';
 // Pages
 import Profile from './pages/rest_profile/rest_profile';
 import Menu from './pages/rest_menu/rest_menu';
+import Login from './pages/Login/Login';
 
 // Components
 import Layout from './components/layout/layout';
+import LocalStorageService from './utils/localStorageService';
 
 class App extends Component {
   state = this.getInitialState();
 
-  getInitialState(){
+  getInitialState() {
     return {
-      menuItemForm: false
-    }
+      menuItemForm: false,
+    };
   }
 
   handleClick = (e) => {
     e.preventDefault();
-    if(e.target.id === "addMenuItem") {
+    if (e.target.id === 'addMenuItem') {
       this.handleMenuItemFormToggle();
     } else return;
-  }
+  };
 
   handleMenuItemFormToggle = () => {
-    if(this.state.menuItemForm) {
-      this.setState({menuItemForm: false});
+    if (this.state.menuItemForm) {
+      this.setState({ menuItemForm: false });
     } else {
-      this.setState({menuItemForm: true});
+      this.setState({ menuItemForm: true });
     }
-  }
+  };
 
-  render(){
+  handleLogout = () => {
+    LocalStorageService.clearToken();
+  };
+
+  render() {
     return (
       <div className="App-Outer-Container">
-        <Layout>
+        <Layout handleLogout={this.handleLogout}>
           <Switch>
-            <Route exact path="/" render={() => 
-              <h1>Home Page Content</h1>
-            }/>
-            <Route exact path="/profile" render={() =>
-              <Profile />
-            }/>
-            <Route exact path="/menu" render={() =>
-              <Menu 
-                menuItemForm = {this.state.menuItemForm}
-                handleClick = {this.handleClick}
-              />
-            }/>
+            <Route exact path="/" render={() => <h1>Home Page Content</h1>} />
+            <Route exact path="/profile" render={() => <Profile />} />
+            <Route
+              exact
+              path="/menu"
+              render={() => (
+                <Menu
+                  menuItemForm={this.state.menuItemForm}
+                  handleClick={this.handleClick}
+                />
+              )}
+            />
+            <Route exact path="/login" render={() => <Login />} />
           </Switch>
         </Layout>
       </div>
