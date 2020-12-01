b3166a46259012e31ae6c49f0b041ca9f583589f
ArthurRyoo <jene0523@gmail.com>
2020-11-30 19:37:04 -0800

    Use kitchenInstance for intercepters

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,15 +1,29 @@
-import axios from "axios";
-import LocalStorageService from "./localStorageService";
+import axios from 'axios';
+import LocalStorageService from './localStorageService';
 
 // LocalstorageService
 const localStorageService = LocalStorageService.getService();
 
-// Add a request interceptor
-axios.interceptors.request.use(
+// var API_URL = 'https://dev.hashdish.com/';
+var API_URL = 'http://local.hashdish.com/';
+if (process.env.NODE_ENV === 'production') {
+  API_URL = 'https://api.hashdish.com/';
+}
+
+const kitchenInstance = axios.create({
+  baseURL: `${API_URL}v1.0/`,
+});
+
+// Add a response interceptor
+kitchenInstance.interceptors.request.use(
   (config) => {
-    console.log('Axios Interceptors');
-    const token = localStorageService.getAuthToken();
-    if (token) {
+    console.log('Axios Request');
+    // if (config.url === 'https://dev.hashdish.com/v1.0/kitchen/refresh') {
+    if (config.url === 'http://local.hashdish.com/v1.0/kitchen/refresh') {
+      const refreshToken = localStorageService.getRefreshToken();
+      config.headers['Authorization'] = 'Bearer ' + refreshToken;
+    } else {
+      const token = localStorageService.getAuthToken();
       config.headers['Authorization'] = 'Bearer ' + token;
     }
     return config;
@@ -19,58 +33,41 @@ axios.interceptors.request.use(
   }
 );
 
-var API_URL = 'https://dev.hashdish.com/'
-if (process.env.NODE_ENV === 'production') {
-  API_URL = 'https://api.hashdish.com/'
-}
-
-//Add a response interceptor
-axios.interceptors.response.use((response) => {
-    return response
-}, function (error) {
+// Add a response interceptor
+kitchenInstance.interceptors.response.use(
+  (response) => {
+    console.log('Axios Response');
+    return response;
+  },
+  function (error) {
     const originalRequest = error.config;
-    if (error.response.status === 401 && originalRequest.url === `${API_URL}v1.0/kitchen/refresh`) {
-    //    router.push('/login'); 
-    // <Redirect to="/login" />
-    return Promise.reject(error);
+    if (
+      error.response.status === 401 &&
+      originalRequest.url === `${API_URL}v1.0/kitchen/refresh`
+    ) {
+      return Promise.reject(error);
     }
 
     if (error.response.status === 401 && !originalRequest._retry) {
-    originalRequest._retry = true;
-    const refreshToken = localStorageService.getRefreshToken();
-    return axios.post(`${API_URL}v1.0/kitchen/refresh`,
-        {
-            "refreshToken": refreshToken
+      originalRequest._retry = true;
+      const refreshToken = localStorageService.getRefreshToken();
+      return kitchenInstance
+        .post(`${API_URL}v1.0/kitchen/refresh`, {
+          refreshToken: refreshToken,
         })
-        .then(res => {
-            if (res.status === 201) {
-                localStorageService.setToken(res.data);
-                axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorageService.getAuthToken();
-                return axios(originalRequest);
-            }
-        })
-}
-return Promise.reject(error);
-});
-
-const kitchenInstance = axios.create({
-  baseURL:`${API_URL}v1.0/`
-});
-
-kitchenInstance.interceptors.request.use(
-  (config) => {
-    console.log('Axios Interceptors');
-    const token = localStorageService.getAuthToken();
-    if (token) {
-      config.headers['Authorization'] = 'Bearer ' + token;
+        .then((res) => {
+          console.log(res.data);
+          console.log(res.status);
+          if (res.status === 200) {
+            localStorageService.setToken(res.data);
+            kitchenInstance.defaults.headers.common['Authorization'] =
+              'Bearer ' + localStorageService.getAuthToken();
+            return axios(originalRequest);
+          }
+        });
     }
-    return config;
-  },
-  (error) => {
-    Promise.reject(error);
+    return Promise.reject(error);
   }
 );
-
-
 
 export default kitchenInstance;
