29a637d01fbf0502f393fa1bd66438425b23578f
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2020-12-18 22:29:22 -0500

    fixes null MenuItemContainer bug

--- src/components/MenuCategory/MenuCategory.jsx
+++ src/components/MenuCategory/MenuCategory.jsx
@@ -7,20 +7,22 @@ const MenuCategory = (props) => {
     <div className={styles.container}>
       <h2>{props.category}</h2>
       <div className={styles.category}>
-        { props.menuItems.map((item, idx) => 
-          <MenuItemContainer 
-            item={item} 
-            idx={idx}
-            category={props.category}
-            selectedMenuItem={props.selectedMenuItem}
-            delMenu={props.delMenu}
-            handleDelMenu={props.handleDelMenu}
-            handleClick={props.handleClick}
-            handleMenuItemEdit={props.handleMenuItemEdit}
-            handleMenuItemUpdate={props.handleMenuItemUpdate}
-            handleMenuItemDelete={props.handleMenuItemDelete}
-            handleMenuItemCancel={props.handleMenuItemCancel}
-          /> 
+        {props.menuItems.map((item, idx) =>
+          item.category === props.category && item.status === 1 ? (
+            <MenuItemContainer
+              item={item}
+              idx={idx}
+              category={props.category}
+              selectedMenuItem={props.selectedMenuItem}
+              delMenu={props.delMenu}
+              handleDelMenu={props.handleDelMenu}
+              handleClick={props.handleClick}
+              handleMenuItemEdit={props.handleMenuItemEdit}
+              handleMenuItemUpdate={props.handleMenuItemUpdate}
+              handleMenuItemDelete={props.handleMenuItemDelete}
+              handleMenuItemCancel={props.handleMenuItemCancel}
+            />
+          ) : null,
         )}
       </div>
       <hr />
